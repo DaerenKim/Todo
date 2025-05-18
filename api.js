c17379a43459cc0fdca75737502ab527e1d24ded
DaerenKim <daerenkim@gmail.com>
2025-05-18 16:23:22 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000', // adjust if your FastAPI is on a different port
+  baseURL: 'URL',
 });
 
 export default api;
